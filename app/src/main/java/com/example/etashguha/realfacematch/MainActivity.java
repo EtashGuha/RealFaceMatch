090e00af21831dcdc747be13260b8c5fcb6e578c
Etash Guha <etashguha@lawn-128-61-122-174.lawn.gatech.edu>
2018-09-08 04:06:02 -0400

    Need to fix jpeg

--- app/src/main/java/com/example/etashguha/realfacematch/MainActivity.java
+++ app/src/main/java/com/example/etashguha/realfacematch/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.etashguha.realfacematch;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+
+import java.io.File;
+
+import id.zelory.compressor.Compressor;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,13 +32,24 @@ public class MainActivity extends AppCompatActivity {
         imgToMimic = findViewById(R.id.imageToMimic);
         btnpic.setOnClickListener(new btnTakePhotoClicker());
         double x = Math.random() * 3;
-        if(x > 2){
+        imgToMimic.setImageResource(R.drawable.frownetash);
+        Bitmap bMap = BitmapFactory.decodeResource(getResources(), R.drawable.frownetash);
+
+        imgToMimic.setImageBitmap(bMap);
+        /*if(x > 2){
             imgToMimic.setImageResource(R.drawable.frownetash);
+            Bitmap bMap = BitmapFactory.decodeResource(getResources(), R.drawable.frownetash);
+            imgToMimic.setImageBitmap(bMap);
         } else if(x < 1){
             imgToMimic.setImageResource(R.drawable.smileakum);
+            Bitmap bMap = BitmapFactory.decodeResource(getResources(), R.drawable.smileakum);
+            imgToMimic.setImageBitmap(bMap);
+
         } else {
             imgToMimic.setImageResource(R.drawable.smileetash);
-        }
+            Bitmap bMap = BitmapFactory.decodeResource(getResources(), R.drawable.smileetash);
+            imgToMimic.setImageBitmap(bMap);
+        }*/
     }
 
     @Override
